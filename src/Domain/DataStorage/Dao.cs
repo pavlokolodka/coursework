f89636defe9e7c5d8ad7b643cee0bd9854bb29ea
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2024-04-20 17:12:40 +0300

    Add JSONDao

--- src/Domain/DataStorage/Dao.cs
+++ src/Domain/DataStorage/Dao.cs
@@ -1,19 +1,12 @@
 ﻿namespace ReserveSpot
 {
-    abstract public class Dao<Entity>
-    {            
-        abstract public Entity FindOne(string id);
-        abstract public List<Entity> FindMany(string id);
-        abstract public bool Delete(string id);
-        abstract public Entity Update(Entity entity);
-    }
-
-    public interface IDao<Entity>
+   public interface IDao<Entity>
     {
-        Entity FindOne(Predicate<Entity> where);
+        Entity? FindOne(Predicate<Entity> where);
         List<Entity> FindMany(Predicate<Entity> where);             
         bool Delete(Predicate<Entity> where);
         Entity Update(Predicate<Entity> where, Entity entity);   
+        Entity Create(Entity entity);   
     }
     //  PropertyPredicate<Entity> filter, Dictionary<string, object> filterProperties
 
